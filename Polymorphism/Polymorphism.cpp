06eb0538d3ef613c43fe773e5f2b1f129f99f20a
Muhammadalif1213 <muhammadalif1213@gmail.com>
2023-05-31 20:22:06 +0700

    mematikan virtual void pesan pada class seseorang

--- Polymorphism/Polymorphism.cpp
+++ Polymorphism/Polymorphism.cpp
@@ -4,11 +4,11 @@ using namespace std;
 class seseorang {
 public:
 	virtual void pesan() = 0;
-	virtual void pesan() {
-		cout << "Pesan dari seseorang" << endl;
-	}
-	virtual void cetak() = 0;
-	virtual void cetak() {
+	//virtual void pesan() {
+		//cout << "Pesan dari seseorang" << endl;
+	//}
+	//virtual void cetak() = 0;
+	void cetak() {
 
 	}
 };
@@ -28,7 +28,6 @@ public:
 	void pesan() {
 		cout << "pesan dari lia" << endl;
 	}
-
 
 };
 
